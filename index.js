e8dff0908ae844c3bc1f2970ad23c039eb48697a
ShonMG <ShonMG@users.noreply.github.com>
2025-07-30 08:11:36 +0000

    deploy: e29cf2e003e84be4f91ab56d657be2a16d76931d

--- index.js
+++ index.js
@@ -85,6 +85,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 var vite_config_default = defineConfig({
+  base: "/atlantis_telkoms_revamp/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
